6c44b1df0babedadb5be0cc26bafa8a2d0496698
Christopher Fox <cfox1290@gmail.com>
2018-03-02 22:04:13 -0700

    most of the code is present or something

--- src/Game/states/boot.js
+++ src/Game/states/boot.js
@@ -1,24 +1,13 @@
-export default function bootState(){
+export default function bootState(game){
     return{
       preload: function() {
-            game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
-            game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
-            game.load.image('sprite','assets/sprites/sprite.png');
+        this.load.image('preloaderBackground', 'img/menu-stuffs/preloadbck.png');
+        this.load.image('preloaderBar', 'img/menu-stuffs/preloadbar.png');
         },
 
         create: function(){
-            Game.playerMap = {};
-            var testKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
-            testKey.onDown.add(Client.sendTest, this);
-            var map = game.add.tilemap('map');
-            map.addTilesetImage('tilesheet', 'tileset'); // tilesheet is the key of the tileset in map's JSON file
-            var layer;
-            for(var i = 0; i < map.layers.length; i++) {
-                layer = map.createLayer(i);
-            }
-            layer.inputEnabled = true; // Allows clicking on the map ; it's enough to do it on the last layer
-            layer.events.onInputUp.add(Game.getCoordinates, this);
-            Client.askNewPlayer();
+            game.canvas.style.cursor = this.cursor; // Sets the pointer to hand sprite
+            game.state.start('load')
         }
     };
 }
